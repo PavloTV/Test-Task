d406f937c3b246a88b8822a516ed221351e955bd
Pavlo Talaver <p2002@inbox.ru>
2018-10-14 21:42:52 +0300

    Output slave public ip

--- credentials.tf
+++ credentials.tf
@@ -3,7 +3,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 resource "aws_key_pair" "deployer" {
-  key_name = "${var.developer}"
+  key_name = "${var.developer}-key"
   public_key = "${file("credentials/ssh/id_rsa.pub")}"
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "master.ip" {
   value = "${aws_instance.master.public_ip}"
 }
+
+output "slave.ip" {
+  value = "${aws_instance.slave.public_ip}"
+}
